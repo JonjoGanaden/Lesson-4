4412e9abc8ba695d6261562848cdc396ba706f6a
JonjoGanaden <jonjoganaden@hotmail.com>
2022-04-07 15:40:29 +0100

    Forms

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtHeight = new System.Windows.Forms.TextBox();
+            this.TxtWidth = new System.Windows.Forms.TextBox();
+            this.LblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnSubmit
+            // 
+            this.BtnSubmit.Location = new System.Drawing.Point(243, 222);
+            this.BtnSubmit.Name = "BtnSubmit";
+            this.BtnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.BtnSubmit.TabIndex = 0;
+            this.BtnSubmit.Text = "Submit";
+            this.BtnSubmit.UseVisualStyleBackColor = true;
+            this.BtnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Width";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtHeight
+            // 
+            this.TxtHeight.Location = new System.Drawing.Point(382, 28);
+            this.TxtHeight.Name = "TxtHeight";
+            this.TxtHeight.Size = new System.Drawing.Size(100, 20);
+            this.TxtHeight.TabIndex = 4;
+            // 
+            // TxtWidth
+            // 
+            this.TxtWidth.Location = new System.Drawing.Point(382, 81);
+            this.TxtWidth.Name = "TxtWidth";
+            this.TxtWidth.Size = new System.Drawing.Size(100, 20);
+            this.TxtWidth.TabIndex = 5;
+            // 
+            // LblAnswer
+            // 
+            this.LblAnswer.AutoSize = true;
+            this.LblAnswer.Location = new System.Drawing.Point(243, 156);
+            this.LblAnswer.Name = "LblAnswer";
+            this.LblAnswer.Size = new System.Drawing.Size(42, 13);
+            this.LblAnswer.TabIndex = 6;
+            this.LblAnswer.Text = "Answer";
+            this.LblAnswer.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblAnswer);
+            this.Controls.Add(this.TxtWidth);
+            this.Controls.Add(this.TxtHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BtnSubmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSubmit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtHeight;
+        private System.Windows.Forms.TextBox TxtWidth;
+        private System.Windows.Forms.Label LblAnswer;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            int width = int.Parse(TxtWidth.Text);
+            int height = int.Parse(TxtHeight.Text);
+            int area = height * width;
+            LblAnswer.Text ="the area of a rectangle is: " + area.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
